8cbf26c9042a649218bd7bb8eae092741c752e26
Rahiman Shaik <rahimanshaik13@gmail.com>
2024-12-11 17:28:29 +0530

    chunk error

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,22 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Simplify imports if needed
+    },
   },
   optimizeDeps: {
-    include: ['react-responsive-carousel']
+    include: ['react-responsive-carousel'], // Pre-bundling dependencies
   },
   build: {
+    chunkSizeWarningLimit: 1000, // Increase the size limit (in KB)
     rollupOptions: {
-      external: ['react-responsive-carousel']
-    }
-  }
+      output: {
+        manualChunks: {
+          // Define chunks for heavy libraries to split them
+          vendor: ['react', 'react-dom', 'react-responsive-carousel'],
+        },
+      },
+    },
+  },
 });
